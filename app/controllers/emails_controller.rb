f13ddb3d6d074dbc43620aeccb935644b2dbc941
jefflunt <jefflunt@gmail.com>
2021-03-31 14:04:17 -0500

    untested controller action. newer ruby. newer bundler

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,24 @@
 class EmailsController < ApplicationController
   def create
+    email = Email.create(email_params(params))
+
+    if email
+      return render(
+        json: { id: email.id },
+        status: :created
+      )
+    else
+      return render(
+        json: { err: { msgs: email.errors.messages } },
+        status: :unprocessable_entity
+      )
+    end
   end
+
+  private
+    def _email_params
+      params
+        .require(:email)
+        .permit(:to, :to_name, :from, :from_name, :subject, :body)
+    end
 end
